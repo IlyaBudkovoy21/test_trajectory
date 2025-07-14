bca9131e45146a2f78fda0ee843404edec536cf3
ilyabudkovoy <ilyabudkovoy@yandex.ru>
2025-07-14 20:41:05 +0300

    feat: Add starting modules
    - Create logger_conf.py for logger configuration
    - Create validators.py
    - Add class Worker

---
+++ logger_conf.py
@@ -0,0 +1,6 @@
+import logging
+
+logging.basicConfig(level=logging.INFO, filename="app.log", format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+
--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+from datetime import datetime
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class Worker:
+    def __init__(self, days: list[dict[str, str | int]], timeslot: list[dict[str, str | int]]):
+        self.days = days.sort(key=lambda day: day["date"])
+        self.timeslot = timeslot.sort(key=lambda day: day["day_id"])
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
